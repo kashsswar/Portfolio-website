f5c70e18e80975e7be7aa90b72bdad01203b6282
Karishma Sinsinwar <kashsswar@gmail.com>
2023-05-04 17:38:55 +0530

    backend_danity

---
+++ schemas/index.js
@@ -0,0 +1,10 @@
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+import experiences from './experiences'
+
+export const schemaTypes = [works, testimonials, brands, abouts, skills, experiences, workExperience, contact]
